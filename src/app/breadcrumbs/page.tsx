24f85ae5a88e87992d8ac5ef1b82f97b74ec2184
Islamiat930 <ademolaislamiat930@gmail.com>
2024-11-01 10:01:29 +0100

    Finished on the icon and breadcrumb, will be completing the table sussequently

---
+++ src/app/breadcrumbs/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Breadcrumbs from '@/components/breadcrumbs/Breadcrumbs';
+
+const BreadcrumbsPage: React.FC = () => {
+  return (
+    <div>
+      <Breadcrumbs />
+    </div>
+  );
+};
+
+export default BreadcrumbsPage;
